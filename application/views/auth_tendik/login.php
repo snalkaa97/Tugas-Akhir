347c1577ab5963ed7b8c8190c3fa8388fbe0829c
sfnalkatiri@gmail.com <sfnalkatiri@gmail.com>
2020-05-05 12:15:59 +0700

    menambahkan fitur login dan registrasi pimpinan tendik

--- application/views/auth_tendik/login.php
+++ application/views/auth_tendik/login.php
@@ -12,22 +12,13 @@
                         <div class="col-lg">
                             <div class="p-5">
                                 <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">Login Pages</h1>
+                                    <h1 class="h4 text-gray-900 mb-4">Login Pimpinan Tendik</h1>
                                 </div>
                                 <?= $this->session->flashdata('message'); ?>
-                                <form class="user" action="<?= base_url('auth') ?>" method="post">
+                                <form class="user" action="<?= base_url('auth/auth_tendik') ?>" method="post">
                                     <div class="form-group">
-                                        <input type="text" class="form-control" id="id" name="id" placeholder="NIP/NIM" value="<?= set_value('nip'); ?>">
-                                        <?= form_error('id', '<small class="text-danger pl-3">', '</small>'); ?>
-                                    </div>
-                                    <div class="form-group">
-                                        <select class="form-control" name="role" id="role">
-                                            <option value="">Pilih Role</option>
-                                            <option value="Pimpinan">Pimpinan</option>
-                                            <option value="Dosen">Dosen</option>
-                                            <option value="Mahasiswa">Mahasiswa</option>
-                                            <option value="LPPM">LPPM</option>
-                                        </select> <?= form_error('role', '<small class="text-danger pl-3">', '</small>'); ?>
+                                        <input type="text" class="form-control" id="nip" name="nip" placeholder="NIP" value="<?= set_value('nip'); ?>">
+                                        <?= form_error('nip', '<small class="text-danger pl-3">', '</small>'); ?>
                                     </div>
                                     <button type="submit" class="btn btn-primary btn-user btn-block">
                                         Login
@@ -38,8 +29,8 @@
                                     <a class="small" href="forgot-password.html">Forgot Password?</a>
                                 </div>
                                 <div class="text-center">
-                                    <a class="small" href="<?= base_url(); ?>auth/registration">Create an Account!</a>
-                                    <a class="small" href="<?= base_url(); ?>auth/auth_tendik">Login Pimpinan Tendik</a>
+                                    <a class="small" href="<?= base_url(); ?>auth/registration_tendik">Create Pimpinan Tendik</a>
+                                    <a class="small" href="<?= base_url(); ?>auth">Login</a>
                                 </div>
                             </div>
                         </div>
