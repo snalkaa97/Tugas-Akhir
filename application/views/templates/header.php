0f92962c987741e5aa4f26809a991cb6fd88ae41
sfnalkatiri@gmail.com <sfnalkatiri@gmail.com>
2020-05-21 15:48:29 +0700

    menambahkan logo bar UMJ

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -10,7 +10,7 @@
     <meta name="author" content="">
 
     <title><?= $title ?></title>
-
+    <link rel="icon" href="<?= base_url('assets/img/UNIVERSITAS MUHAMMADIYAH JAKARTA.png') ?>" type="image/x-icon">
     <!-- Custom fonts for this template-->
     <link href="<?= base_url('assets/'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="<?= base_url('assets/'); ?>https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
